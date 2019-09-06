58ffbaed0235d5ae13d391a30e2844e252112529
Michael Hughes <mhughes36@live.co.uk>
2019-09-06 21:08:00 +0100

    about page snapshot test

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
     <div>
       <h1>About</h1>
-      <p>In an effort to help extend Health & Safety knowledge to a variety of languages, this is an app that translates terminology.</p>
+      <p>
+        In an effort to help extend Health & Safety knowledge to a variety of
+        languages, this is an app that translates terminology.
+      </p>
     </div>
-  )
+  );
 }
 
 export default About;
--- src/components/pages/About.test.js
+++ src/components/pages/About.test.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import About from './About';
-import { shallow } from 'enzyme';
+import React from "react";
+import About from "./About";
+import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 
-describe('About', () => {
-  it('displays about page', () => {
-    const wrapper = shallow(<About />)
-    const heading = wrapper.find('h1').text()
-    expect(heading).toEqual('About')
-  })
-})
+describe("About", () => {
+  it("displays about page", () => {
+    const wrapper = shallow(<About />);
+    const heading = wrapper.find("h1").text();
+    expect(heading).toEqual("About");
+  });
+
+  it("matches the snapshot", () => {
+    const tree = renderer.create(<About />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
