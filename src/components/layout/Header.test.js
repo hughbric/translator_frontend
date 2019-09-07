3c7cfb21fbd30b5d485ca37f569172988b87da46
Michael Hughes <mhughes36@live.co.uk>
2019-09-07 11:24:05 +0100

    app snapshot test

--- src/components/layout/Header.test.js
+++ src/components/layout/Header.test.js
@@ -1,13 +1,13 @@
-import React from "react";
-import Header from "./Header";
-import { shallow } from "enzyme";
+import React from 'react';
+import Header from './Header';
+import { shallow } from 'enzyme';
 
-describe("Header", () => {
-  it("displays headings", () => {
+describe('<Header />', () => {
+  it('shallow renders headings', () => {
     const wrapper = shallow(<Header />);
-    const heading = wrapper.find("h1").text();
-    const subheading = wrapper.find("h2").text();
-    expect(heading).toEqual("Health & Safety");
-    expect(subheading).toEqual("Translator");
+    const heading = wrapper.find('h1').text();
+    const subheading = wrapper.find('h2').text();
+    expect(heading).toEqual('Health & Safety');
+    expect(subheading).toEqual('Translator');
   });
 });
--- src/components/pages/About.test.js
+++ src/components/pages/About.test.js
@@ -1,16 +1,16 @@
-import React from "react";
-import About from "./About";
-import { shallow } from "enzyme";
-import renderer from "react-test-renderer";
+import React from 'react';
+import About from './About';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
-describe("About", () => {
-  it("displays about page", () => {
+describe('<About />', () => {
+  it('contains a heading', () => {
     const wrapper = shallow(<About />);
-    const heading = wrapper.find("h1").text();
-    expect(heading).toEqual("About");
+    const heading = wrapper.find('h1').text();
+    expect(heading).toEqual('About');
   });
 
-  it("matches the snapshot", () => {
+  it('matches the snapshot', () => {
     const tree = renderer.create(<About />).toJSON();
 
     expect(tree).toMatchSnapshot();
